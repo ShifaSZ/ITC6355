9994a1baf25c24cc7bf08518dfcf367182d99cd7
Josezs <21203192@qq.com>
2018-12-14 11:28:00 -0500

    Updating jsonData

--- web/js/jsondata.js
+++ web/js/jsondata.js
@@ -6,6 +6,12 @@ let jsonData = {
              "Chapter 1 - Introduction to HTML",
                 "Chapter 2 - HTML Elements",
                 "Chapter 3 - HTML5"
+            ],
+            "chapterDesc":[
+            "HyperText Markup Language, commonly abbreviated as HTML, is the standard markup language used to create web pages. Along with CSS, and JavaScript, HTML is a cornerstone technology used to create web pages as well as to create user interfaces for mobile and web applications. Taught by an instructor with over 15 years of real world web development experience. This course has everything you need to learn how write HTML code from scratch. Fast paced get to the point training, to learn about creating websites and using HTML code. By the end of the course you will have the skills and know how to apply HTML to make a real website.",
+            "Welcome to your very first beginning tutorial on CSS and this is part of our ongoing web development series now if you do not have a solid understanding of HTML you should go to my channel page and go through the HTML for beginners tutorial set that's kind of a prerequisite now if you already know HTML and you feel you don't need that that's fine but if you don't know anything about HTML I strongly recommend that you go ahead and take that series because an understanding of HTML is really crucial to using CSS now as a note we will also be introducing some new HTML elements in this series and the reason is they go hand-in-hand together so it didn't make sense to discuss those in the HTML for beginners because they really work hand in hand with CSS in terms of what you will need we're just gonna use notepad for now just like we did in the HTML for beginners series now eventually we'll use an editor but for now we just need no pen okay so what is CSS well here's the dictionary version CSS stands for cascading style sheets.",
+            "JavaScript (also known as JS) is one of the core pillars in developing for the modern web. It allows you to define how users can interact with your applications on the client-side and can also be used to define the server code. Javascript is one of the major components of building web applications. We have HTML for deciding what the content is, we have CSS to decide what the content looks like, and then we have JavaScript to provide interactivity to our users. JavaScript is still a programming language, so in this course we’ll introduce you to the basics and show you how to get started integrating JS in your applications!"
+
             ]
     }, {
             "name": "CSS",
@@ -13,14 +19,28 @@ let jsonData = {
              "Chapter 1 - Introduction to CSS",
                 "Chapter 2 - CSS Elements",
                 "Chapter 3 - Advanced CSS"
+            ],
+            "chapterDesc":[
+            "HyperText Markup Language, commonly abbreviated as HTML, is the standard markup language used to create web pages. Along with CSS, and JavaScript, HTML is a cornerstone technology used to create web pages as well as to create user interfaces for mobile and web applications. Taught by an instructor with over 15 years of real world web development experience. This course has everything you need to learn how write HTML code from scratch. Fast paced get to the point training, to learn about creating websites and using HTML code. By the end of the course you will have the skills and know how to apply HTML to make a real website.",
+            "Welcome to your very first beginning tutorial on CSS and this is part of our ongoing web development series now if you do not have a solid understanding of HTML you should go to my channel page and go through the HTML for beginners tutorial set that's kind of a prerequisite now if you already know HTML and you feel you don't need that that's fine but if you don't know anything about HTML I strongly recommend that you go ahead and take that series because an understanding of HTML is really crucial to using CSS now as a note we will also be introducing some new HTML elements in this series and the reason is they go hand-in-hand together so it didn't make sense to discuss those in the HTML for beginners because they really work hand in hand with CSS in terms of what you will need we're just gonna use notepad for now just like we did in the HTML for beginners series now eventually we'll use an editor but for now we just need no pen okay so what is CSS well here's the dictionary version CSS stands for cascading style sheets.",
+            "JavaScript (also known as JS) is one of the core pillars in developing for the modern web. It allows you to define how users can interact with your applications on the client-side and can also be used to define the server code. Javascript is one of the major components of building web applications. We have HTML for deciding what the content is, we have CSS to decide what the content looks like, and then we have JavaScript to provide interactivity to our users. JavaScript is still a programming language, so in this course we’ll introduce you to the basics and show you how to get started integrating JS in your applications!"
+
             ]
+
     }, {
             "name": "JAVASCRIPT",
             "chapters": [
              "Chapter 1 - Introduction to JS",
                 "Chapter 2 - JS",
                 "Chapter 3 - Advanced JS"
+            ],
+            "chapterDesc":[
+            "HyperText Markup Language, commonly abbreviated as HTML, is the standard markup language used to create web pages. Along with CSS, and JavaScript, HTML is a cornerstone technology used to create web pages as well as to create user interfaces for mobile and web applications. Taught by an instructor with over 15 years of real world web development experience. This course has everything you need to learn how write HTML code from scratch. Fast paced get to the point training, to learn about creating websites and using HTML code. By the end of the course you will have the skills and know how to apply HTML to make a real website.",
+            "Welcome to your very first beginning tutorial on CSS and this is part of our ongoing web development series now if you do not have a solid understanding of HTML you should go to my channel page and go through the HTML for beginners tutorial set that's kind of a prerequisite now if you already know HTML and you feel you don't need that that's fine but if you don't know anything about HTML I strongly recommend that you go ahead and take that series because an understanding of HTML is really crucial to using CSS now as a note we will also be introducing some new HTML elements in this series and the reason is they go hand-in-hand together so it didn't make sense to discuss those in the HTML for beginners because they really work hand in hand with CSS in terms of what you will need we're just gonna use notepad for now just like we did in the HTML for beginners series now eventually we'll use an editor but for now we just need no pen okay so what is CSS well here's the dictionary version CSS stands for cascading style sheets.",
+            "JavaScript (also known as JS) is one of the core pillars in developing for the modern web. It allows you to define how users can interact with your applications on the client-side and can also be used to define the server code. Javascript is one of the major components of building web applications. We have HTML for deciding what the content is, we have CSS to decide what the content looks like, and then we have JavaScript to provide interactivity to our users. JavaScript is still a programming language, so in this course we’ll introduce you to the basics and show you how to get started integrating JS in your applications!"
+
             ]
+
     }
     ]
 };
